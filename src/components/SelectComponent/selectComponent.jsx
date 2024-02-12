ebe9b64e88192da14a36a5c6ca586710d6dbf139
Moonshield7 <fannie.p@hotmail.fr>
2024-02-12 10:13:27 +0100

    🚀 Deploying old version

--- src/components/SelectComponent/selectComponent.jsx
+++
@@ -1,8 +0,0 @@
-function SelectComponent ({opts, type}) {
-  return (
-    <select name={type} id={type} className="custom-select">
-      {opts.map(opt => <option key={opt} value={opt}>{opt}</option> )}
-    </select>
-  );
-}
-export default SelectComponent;
